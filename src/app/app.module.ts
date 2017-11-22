aa1881a53ec557667a216a044736741006d51e01
lubgade <lubgade@yahoo.com>
2017-11-22 11:14:05 -0500

    feat: Add login and auth components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule, JsonpModule} from '@angular/http';
+import {BootstrapModalModule} from 'ng2-bootstrap-modal';
+import {Cookie} from 'ng2-cookies/ng2-cookies';
 
 import { AppComponent } from './app.component';
 import {NavbarComponent} from './components/navbar/navbar.components';
 import {AboutComponent} from './components/about/about.component';
 import {SearchComponent} from './components/search/search.component';
 import {routing} from './app.routes';
+import {LoginComponent} from './components/login/login.component';
+import {ModalComponent} from './components/modal/modal.component';
+import {SpotifyService} from './services/spotify.service';
+import {AuthGuard} from '../app/components/Auth/auth.guard';
+import {AuthService} from '../app/components/Auth/auth.service';
 
 
 @NgModule({
@@ -14,14 +22,19 @@ import {routing} from './app.routes';
     AppComponent,
     NavbarComponent,
     AboutComponent,
-    SearchComponent
+    SearchComponent,
+    LoginComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpModule,
+    JsonpModule,
+    BootstrapModalModule,
   ],
-  providers: [],
+  providers: [SpotifyService, AuthService, AuthGuard, Cookie],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
