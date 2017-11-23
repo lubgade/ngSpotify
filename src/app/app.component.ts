b2f8dcd08a163f229f3a4d3c9a3d95156212724a
lubgade <lubgade@yahoo.com>
2017-11-23 18:35:37 -0500

    feat: Add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import {AboutComponent} from './components/about/about.component';
-import {SearchComponent} from './components/search/search.component';
 import {NavbarComponent} from './components/navbar/navbar.components';
 import {LoginComponent} from './components/login/login.component';
-import {ModalComponent} from './components/modal/modal.component';
 
 @Component({
   moduleId: module.id,
