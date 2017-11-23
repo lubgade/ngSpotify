f5ac11afccbc4402b2ba5ed2034bb3395e26ab5e
lubgade <lubgade@yahoo.com>
2017-11-23 18:51:08 -0500

    feat: Implement routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,29 @@ import {AboutComponent} from './components/about/about.component';
 import {LoginComponent} from './components/login/login.component';
 import {ModalComponent} from './components/modal/modal.component';
 import {AuthGuard} from '../app/components/Auth/auth.guard';
+import {ArtistComponent} from '../app/components/artist/artist.component';
 
 const routes: Routes = [
-    {
+    /*{
         path : '',
         component: AboutComponent
-    },
+    },*/
     {
         path: 'login',
         component: LoginComponent,
     },
     {
-        path: 'search',
+        path: '',
         component: SearchComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: '**',
         redirectTo: ''
+    },
+    {
+        path: 'artist/:id',
+        component: ArtistComponent
     }
 ];
 
