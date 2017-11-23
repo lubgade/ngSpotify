d6254153917dd45bc6fb58ec7d5139a90d9709ed
lubgade <lubgade@yahoo.com>
2017-11-23 18:39:09 -0500

    feat: Implement canActivate on search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {AuthService} from '../Auth/auth.service';
+import {Observable} from 'rxjs/Observable';
+import {Artist} from '../../../Artist';
 
 @Component({
   moduleId: module.id,  
   selector: 'search',
   templateUrl: 'search.component.html',
+  providers: [SpotifyService]
 })
-export class SearchComponent {
-  searchStr: string;
+
+export class SearchComponent implements OnInit{
+  private searchstr: string;
+  access_token: string;
+  refresh_token: string;
+  isLoggedIn$: Observable<boolean>;
+  searchRes: Artist[];
+
+  constructor(private spotifyService: SpotifyService, 
+              private router: Router, 
+              private authService:AuthService){
+  }
+
+  ngOnInit(){
+    this.isLoggedIn$ = this.authService.isLoggedIn;  
+  }
+
   
   searchMusic(){
-    console.log(this.searchStr);
+    this.access_token = localStorage.getItem('access_token');
+    this.spotifyService.searchMusic(this.searchstr, 'artist', this.access_token).
+    subscribe(res =>{
+        this.searchRes = res.artists.items;
+    });
   }
+
 }
