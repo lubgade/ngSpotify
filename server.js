38d2edce43a13d0c30576b7afd01bd4b9393a721
lubgade <lubgade@yahoo.com>
2017-11-30 17:47:54 -0500

    feat: change server.js

--- server.js
+++ server.js
@@ -65,6 +65,20 @@ var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+
+app.use(forceSSL());
+
+
 //app.use(express.static(__dirname + 'public'))
 
 app.use(express.static(path.join(__dirname, 'dist')));
